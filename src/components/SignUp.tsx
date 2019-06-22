ce52a37b9bdfc453cf93dd905c602f671e75869e
Jordantsui <jiehaocui@foxmail.com>
2019-06-22 15:37:13 +0800

    完成登录页面

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { Input, Icon, Button } from 'antd';
-// import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import axios from 'src/config/axios'
 
-// import './SignUp.scss'
+import './SignUp.scss'
 
 interface ISignUpState {
 	account: string,
@@ -42,7 +42,7 @@ class SignUp extends React.Component<any,ISignUpState> {
 				password,
 				password_confirmation:passwordConformation
 			})
-			// this.props.history.push('/')
+			this.props.history.push('/')
 			console.log('成功')
 		}catch(e){
 			throw new Error(e)
@@ -52,10 +52,10 @@ class SignUp extends React.Component<any,ISignUpState> {
 
 	public render() {
 		const { account,password,passwordConformation } = this.state;
-		// <h1>番茄闹钟毕设注册</h1>
-		// <p>如果你有账号，请立即 <Link to="/login">登录</Link></p>
+		
 		return (
 			<div className="SignUp" id="SignUp">
+				<h1>番茄闹钟毕设注册</h1>
 				<Input
 					placeholder="请输入你的用户名"
 					prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
@@ -65,6 +65,7 @@ class SignUp extends React.Component<any,ISignUpState> {
 				<Input.Password value={password} placeholder="请输入密码" onChange={this.onChangePassword}/>
 				<Input.Password value={passwordConformation} placeholder="请确认密码" onChange={this.onChangePasswordConformation}/>
 				<Button type="primary" className="signUpButton" onClick={this.submit}>注册</Button>
+				<p>如果你有账号，请立即 <Link to="/login">登录</Link></p>
 			</div>
 		);
 	}
