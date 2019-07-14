4fdb1c9d1ba9afbfd549a21184955906b8fe8f0c
Jordantsui <jiehaocui@foxmail.com>
2019-07-14 10:57:09 +0800

    添加addTodo到TodoInput组件

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import {addTodo} from "../../redux/actions";
+import axios from 'src/config/axios';
 import {Input,Icon} from 'antd';
 
 interface ITodoInputState {
@@ -6,7 +9,8 @@ interface ITodoInputState {
 }
 
 interface ITodoInputProps {
-	addTodo: (params:any) => void;
+	/* addTodo: (params:any) => void; */
+	addTodo: (payload:any) => any;
 }
 
 class TodoInput extends React.Component<ITodoInputProps,ITodoInputState> {
@@ -15,23 +19,32 @@ class TodoInput extends React.Component<ITodoInputProps,ITodoInputState> {
 		this.state = {
 			description: ''
 		}
+		console.log(this.props)
 	}
 
 	onKeyUp = (e) => {
 		if(e.keyCode === 13 && this.state.description !== ''){
-			this.addTodo()
+			//this.addTodo()
+			this.postTodo()
 		}
 		// 13表示回车键
 	}
 
-	addTodo = ()=>{
-		this.props.addTodo({description: this.state.description})
+/* 	addTodo = ()=>{
+		this.props.addTodo({description: this.state.description}) */
+	postTodo = async ()=>{
+		try {
+			const response = await axios.post('todos',{description: this.state.description})
+			this.props.addTodo(response.data.resource)
+		}catch (e) {
+			throw new Error(e)
+		}
 		this.setState({description: ''})
 	}
 
 	public render() {
 		const { description } = this.state;
-		const suffix = description ? <Icon type="enter" onClick={this.addTodo}/> : <span/>;
+		const suffix = description ? <Icon type="enter" onClick={this.postTodo}/> : <span/>;
 		return (
       <div className="TodoInput" id="TodoInput">
 	      <Input
@@ -46,4 +59,13 @@ class TodoInput extends React.Component<ITodoInputProps,ITodoInputState> {
 	}
 }
 
-export default TodoInput;
+const mapStateToProps = (state, ownProps) => ({
+	...ownProps
+})
+
+const mapDispatchToProps = {
+	addTodo
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(TodoInput);
+//export default TodoInput;
